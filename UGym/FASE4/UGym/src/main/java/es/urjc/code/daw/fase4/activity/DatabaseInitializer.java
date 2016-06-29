61c089690c9434e5b2dc844f5c3cc220329290d9
HDominguezD <hector_sdr.rm_vk@hotmail.com>
2016-06-29 11:41:42 +0200

    datos iniciales de instalaciones

--- UGym/FASE4/UGym/src/main/java/es/urjc/code/daw/fase4/activity/DatabaseInitializer.java
+++ UGym/FASE4/UGym/src/main/java/es/urjc/code/daw/fase4/activity/DatabaseInitializer.java
@@ -26,8 +26,13 @@ public class DatabaseInitializer implements CommandLineRunner {
 		activityRepository.save(new Activity("SPINNING","Forma parte del pelotón ciclista liderado por nuestros mejores profesionales. Recorreras un sin fín de emociones con la mejor música y las más originales clases temáticas."));
 		activityRepository.save(new Activity("ZUMBA","Un entrenamiento completo que combina todos los elementos del acondicionamiento físico: trabajo cardiovascular y muscular, equilibrio y flexibilidad, aumento de energía y muchísimos beneficios increíbles que disfrutarás después de cada clase."));
 
-		
-		
+		// Sample Installations
+
+		installationRepository.save(new Installation(1,'Sala de máquinas','Más de 150 m2  de zona de entrenamiento, pensada para que puedas entrenar con libertad y con todo tipo de equipamiento. Máquinas con carga de discos, poleas, barras, un gran número de mancuernas y bancos para que no esperes. Muchos deportistas se deciden por nuestras instalaciones por la cantidad y variedad de equipamiento que hay a disposición.Dispondrás de una zona de entrenamiento cardiovascular con equipamiento de última generación. Más de 70 equipos de cardio: cintas de correr, elípticas, steps, remos, bicicletas de diferentes tipos, incluso para personas que padezcan lesiones de espalda o tren inferior. Además tendrás la posibilidad de ver tu programa favorito de TV mientras entrenas, escuchar tu música conectando tu ipod a las máquinas, etc., conocerás una manera más amena y divertida de entrenar.', 'sala_maquinas.jpg'));
+    installationRepository.save(new Installation(2,'Piscina','En nuestra piscina de agua salada podrás practicar la natación por libre o bien asistir a uno de los cursos de natación de nuestra amplia oferta. Disponemos de cursos tanto de iniciación como de perfeccionamiento o entrenamiento, para bebes, niños y adultos. También ofrecemos cursos para embarazadas y cursos de matronatación, que favorecen el equilibrio, la coordinación, el desarrollo psicomotor, la creatividad y la fuerza muscular del bebe, además de reforzar el vínculo entre los padres y el bebé.', 'piscina.jpg'));
+    installationRepository.save(new Installation(3,'Sauna','Este medio de relajación ofrece incontables ventajas: desintoxica la piel, purifica el organismo, dilata los bronquios, regula la presión sanguínea, mejora el ritmo cardíaco y circulatorio.','sauna.jpg'));
+
+
 		// Sample users
 
 		userRepository.save(new User("user", "pass", "ROLE_USER"));
